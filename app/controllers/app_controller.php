9b018b7c75840fb029672945c182188d829b3762
Thomas Lemée <thomlemee@gmail.com>
2015-02-14 15:19:43 +0100

    front

--- app/controllers/app_controller.php
+++ app/controllers/app_controller.php
@@ -4,40 +4,41 @@ namespace APP\CONTROLLERS;
 
 class app_controller{
 
-  private $tpl;
-  private $model;
-  private $dataset;
+	private $tpl;
+	private $model;
+	private $dataset;
 
-  function __construct(){
-    $this->tpl='main.html';
-    $this->model=new \APP\MODELS\app_model();
-  }
+	function __construct(){
+		$this->tpl='main.html';
+		$this->model=new \APP\MODELS\app_model();
+	}
 
-  function home(){
+	function home(){
 
-  }
+	}
 
-  function getVenue($f3, $params){
-    $this->dataset=$this->model->getVenue($params);
-    $f3->set('one',$this->dataset);
-  }
+	function getVenue($f3, $params){
+		$this->dataset=$this->model->getVenue($params);
+		$f3->set('one',$this->dataset);
+		$this->tpl='venue.html';
+	}
 
-  function imageResize($f3, $params){
-    $size = explode('x', $params['size']);
-    $folder = $params['folder'];
-    $file = $params['file'];
-    $img = new \Image('/public/upload/'.$folder.'/'.$file);
-    $img->resize($size[0],$size[1]);
-    $this->tpl='image_render.html';
-    $f3->set('img',$img);
-  }
+	function imageResize($f3, $params){
+		$size = explode('x', $params['size']);
+		$folder = $params['folder'];
+		$file = $params['file'];
+		$img = new \Image('/public/upload/'.$folder.'/'.$file);
+		$img->resize($size[0],$size[1]);
+		$this->tpl='image_render.html';
+		$f3->set('img',$img);
+	}
 
-  function afterroute(){
-    //echo View::instance()->render($this->tpl);
-    //$template=new Template;
-    //echo Template::instance()->render('main.html');
-    echo \View::instance()->render($this->tpl);
-  }
+	function afterroute(){
+		//echo View::instance()->render($this->tpl);
+		//$template=new Template;
+		//echo Template::instance()->render('main.html');
+		echo \View::instance()->render($this->tpl);
+	}
 
 }
 
---
+++ app/views/partials/header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+		<title></title>
+		<link href='http://fonts.googleapis.com/css?family=Roboto:400,300,500' rel='stylesheet' type='text/css'>
+		<link rel="stylesheet" href="/public/stylesheet/fonts.css">
+		<link rel="stylesheet" href="/public/stylesheet/style.css">
+
+		<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDaz-DUqja-r1rHgXJRTDBwK7JYkYuSkYc&sensor=false"></script>
+
+	</head>
+
+	<body>
+
+		<header>
+			<div class="globalAlign">
+				<ul class="headerMenu">
+					<li class="activeHeaderLink"><a href="#">Lieux</a></li>
+					<li><a href="#">Mes réservations</a></li>
+					<li><a href="#">Recherche avancée</a></li>
+				</ul>
+
+				<div class="headerProfile">
+					<a href="#">
+						<span class="headerProfileName">Arthur</span>
+						<span class="headerProfilePic"></span>
+					</a>
+				</div>
+			</div>
+		</header>
